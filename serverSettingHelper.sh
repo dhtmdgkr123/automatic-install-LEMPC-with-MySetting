00bc49b5fd38962b8f403cb2a7dd07eaa638ea95
matas <osh12201@gmail.com>
2019-06-30 20:47:18 +0900

    edit redis installScript
    edit redis installScript

--- serverSettingHelper.sh
+++ serverSettingHelper.sh
@@ -66,7 +66,6 @@ installCodeigniter() {
     moveFiles &&
     removeVendorFile &&
     cd $NOW && overWriteFile $NOW
-    
     service nginx restart
 }
 
@@ -123,12 +122,121 @@ installRedis() {
     make distclean &&
     make &&
     sudo make install &&
+
+
+    SCRIPT=$(readlink -f $0)
+    SCRIPTPATH=$(dirname $SCRIPT)
+
+    REDIS_PORT=6379
+    REDIS_CONFIG_FILE="/etc/redis/$REDIS_PORT.conf"
+    REDIS_LOG_FILE="/var/log/redis_$REDIS_PORT.log"
+    REDIS_DATA_DIR="/var/lib/redis/$REDIS_PORT"
+    REDIS_EXECUTABLE=`command -v redis-server`
+    CLI_EXEC=`dirname $REDIS_EXECUTABLE`"/redis-cli"
+    mkdir -p `dirname "$REDIS_CONFIG_FILE"`
+    mkdir -p `dirname "$REDIS_LOG_FILE"`
+    mkdir -p "$REDIS_DATA_DIR"
+
+
+    TMP_FILE="/tmp/${REDIS_PORT}.conf"
+    DEFAULT_CONFIG="${SCRIPTPATH}/../redis.conf"
+    INIT_TPL_FILE="${SCRIPTPATH}/redis_init_script.tpl"
+    INIT_SCRIPT_DEST="/etc/init.d/redis_${REDIS_PORT}"
+    PIDFILE="/var/run/redis_${REDIS_PORT}.pid"
+    
+read -r SED_EXPR <<-EOF
+s#^port .\+#port ${REDIS_PORT}#; \
+s#^logfile .\+#logfile ${REDIS_LOG_FILE}#; \
+s#^dir .\+#dir ${REDIS_DATA_DIR}#; \
+s#^pidfile .\+#pidfile ${PIDFILE}#; \
+s#^daemonize no#daemonize yes#;
+EOF
+sed "$SED_EXPR" $DEFAULT_CONFIG >> $TMP_FILE
+cp $TMP_FILE $REDIS_CONFIG_FILE
+rm -f $TMP_FILE
+
+    REDIS_INIT_HEADER=\
+    "#!/bin/sh\n
+    #Configurations injected by install_server below....\n\n
+    EXEC=$REDIS_EXECUTABLE\n
+    CLIEXEC=$CLI_EXEC\n
+    PIDFILE=\"$PIDFILE\"\n
+    CONF=\"$REDIS_CONFIG_FILE\"\n\n
+    REDISPORT=\"$REDIS_PORT\"\n\n
+    ###############\n\n"
+
+    REDIS_CHKCONFIG_INFO=\
+    "# REDHAT chkconfig header\n\n
+    # chkconfig: - 58 74\n
+    # description: redis_${REDIS_PORT} is the redis daemon.\n
+    ### BEGIN INIT INFO\n
+    # Provides: redis_6379\n
+    # Required-Start: \$network \$local_fs \$remote_fs\n
+    # Required-Stop: \$network \$local_fs \$remote_fs\n
+    # Default-Start: 2 3 4 5\n
+    # Default-Stop: 0 1 6\n
+    # Should-Start: \$syslog \$named\n
+    # Should-Stop: \$syslog \$named\n
+    # Short-Description: start and stop redis_${REDIS_PORT}\n
+    # Description: Redis daemon\n
+    ### END INIT INFO\n\n"
+
+    if command -v chkconfig >/dev/null; then
+        echo "$REDIS_INIT_HEADER" "$REDIS_CHKCONFIG_INFO" > $TMP_FILE && cat $INIT_TPL_FILE >> $TMP_FILE || die "Could not write init script to $TMP_FILE"
+    else
+        echo "$REDIS_INIT_HEADER" > $TMP_FILE && cat $INIT_TPL_FILE >> $TMP_FILE || die "Could not write init script to $TMP_FILE"
+    fi
+
+cat > ${TMP_FILE} <<EOT
+#!/bin/sh
+#Configurations injected by install_server below....
+
+EXEC=$REDIS_EXECUTABLE
+CLIEXEC=$CLI_EXEC
+PIDFILE=$PIDFILE
+CONF="$REDIS_CONFIG_FILE"
+REDISPORT="$REDIS_PORT"
+###############
+# SysV Init Information
+# chkconfig: - 58 74
+# description: redis_${REDIS_PORT} is the redis daemon.
+### BEGIN INIT INFO
+# Provides: redis_${REDIS_PORT}
+# Required-Start: \$network \$local_fs \$remote_fs
+# Required-Stop: \$network \$local_fs \$remote_fs
+# Default-Start: 2 3 4 5
+# Default-Stop: 0 1 6
+# Should-Start: \$syslog \$named
+# Should-Stop: \$syslog \$named
+# Short-Description: start and stop redis_${REDIS_PORT}
+# Description: Redis daemon
+### END INIT INFO
+
+EOT
+    cat ${INIT_TPL_FILE} >> ${TMP_FILE}
+
+    cp $TMP_FILE $INIT_SCRIPT_DEST && chmod +x $INIT_SCRIPT_DEST
+
+
+    if command -v chkconfig >/dev/null 2>&1; then
+        chkconfig --add redis_${REDIS_PORT}
+        chkconfig --level 345 redis_${REDIS_PORT} on
+    elif command -v update-rc.d >/dev/null 2>&1; then
+        update-rc.d redis_${REDIS_PORT} defaults
+
+
+    /etc/init.d/redis_$REDIS_PORT start
+
     cd ~ &&
     rm -rf redis-stable redis-stable.tar.gz &&
     cd $1
     if ! packageExists php-redis; then
         installPackage php-redis
-    fi
+    fi &&
+
+
+
+    service php7.3-fpm restart
 }
 
 clear && 
